0b35cd57e283dd82a8f462ee359b4da16609b8ec
Eric M <eric.marchese@web.com>
2019-02-22 14:22:43 -0500

    Basic working app

--- MimosaApp/AppDelegate.swift
+++ MimosaApp/AppDelegate.swift
@@ -1,46 +1,53 @@
-//
-//  AppDelegate.swift
-//  MimosaApp
-//
-//  Created by Eric Marchese on 2019-02-22.
-//  Copyright © 2019 Acquisio. All rights reserved.
-//
-
 import UIKit
+import Turbolinks
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    var navigationController = UINavigationController()
+    var session = Session()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window?.rootViewController = navigationController
+        session.delegate = self
+        visit(URL: URL(string: "https://mimosaworks.com")!, action: .Advance)
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    
+    func visit(URL: URL, action: Action) {
+        //        let visitable = VisitableViewController(url: URL)
+        //
+        //        navigationController.pushViewController(visitable, animated: true)
+        //        session.visit(visitable)
+        
+        
+        let visitable = VisitableViewController(url: URL)
+        switch action {
+        case .Advance:
+            navigationController.pushViewController(visitable, animated: true)
+            break
+        case .Replace:
+            navigationController.popViewController(animated: false)
+            navigationController.pushViewController(visitable, animated: false)
+            break
+        default:
+            navigationController.pushViewController(visitable, animated: true)
+            break
+        }
+        session.visit(visitable)
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
+    
 }
 
+extension AppDelegate: SessionDelegate {
+    func session(_ session: Session, didProposeVisitToURL URL: URL, withAction action: Action) {
+        visit(URL: URL, action: action)
+    }
+    
+    func session(_ session: Session, didFailRequestForVisitable visitable: Visitable, withError error: NSError) {
+        let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        navigationController.present(alert, animated: true, completion: nil)
+    }
+}
